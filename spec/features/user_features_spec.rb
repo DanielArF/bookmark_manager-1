b1b7860be821ea13192fb797fe06fb880a49bdc3
Christopher Comella <git.chrisco@gmail.com>
2015-09-25 11:17:09 +0200

    WIP: 'registered user can sign in with vaild credentials'

--- spec/features/user_features_spec.rb
+++ spec/features/user_features_spec.rb
@@ -15,7 +15,7 @@ feature 'register a new user' do
       expect(User.count).to eq 1
 	end
 
-	scenario 'new user not created with vaild input ' do
+	scenario 'new user not created with invaild input' do
       expect(User.count).to eq 0
       visit '/users/new'
       fill_in 'name', with: ''
@@ -24,16 +24,16 @@ feature 'register a new user' do
       fill_in 'password_confirm', with: 'anything'
       click_button 'Register'
       expect(User.count).to eq 0
-  end 
+  end
 
-    scenario 'logs in new user with valid credentials ' do
+    scenario 'registered user can sign in with vaild credentials' do
       User.create(name: 'bo', email: 'bo@cint.com', password: 'password', password_confirm: 'password')
       visit '/users/signin'
       fill_in 'email', with: 'bo@cint.com'
       fill_in 'password', with: 'password'
-      click_button 'Register'
+      click_button 'Sign In'
       expect(page.current_path).to eq '/'
-      expect(page).to have_content "Welcome bo@cint,com"
-  end 
+      expect(page).to have_content "Welcome bo@cint.com"
+  end
 
 end
